94facaad8cb8edf12b816546ec63e139bf4155cf
Jahanzaibkhan07 <ak68681825@gmail.com>
2022-11-16 19:14:43 +0500

    make it better

--- index.js
+++ index.js
@@ -29,4 +29,4 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "public", "index.html"));
 });
 
-app.listen(port, console.log(`Server running on port ${port}`));
+app.listen(port, console.log(`Server running on Port ${port}`));
